2205e99942fa161cad41f88d3efd4e7f48b21f0b
mrbuche <dev@mrbuche.com>
2025-02-16 16:59:49 -0700

    separate to use in book

--- src/constitutive/solid/hyperelastic/mooney_rivlin/mod.rs
+++ src/constitutive/solid/hyperelastic/mooney_rivlin/mod.rs
@@ -3,24 +3,7 @@ mod test;
 
 use super::*;
 
-/// The Mooney-Rivlin hyperelastic constitutive model.[^mooney1940]<sup>,</sup>[^rivlin1948]
-///
-/// [^mooney1940]: M. Mooney, [J. Appl. Phys. **11**, 582 (1940)](https://doi.org/10.1063/1.1712836).
-/// [^rivlin1948]: R.S. Rivlin, [Philos. Trans. R. Soc. London, Ser. A **241**, 379 (1948)](https://doi.org/10.1098/rsta.1948.0024).
-///
-/// **Parameters**
-/// - The bulk modulus $`\kappa`$.
-/// - The shear modulus $`\mu`$.
-/// - The extra modulus $`\mu_m`$.
-///
-/// **External variables**
-/// - The deformation gradient $`\mathbf{F}`$.
-///
-/// **Internal variables**
-/// - None.
-///
-/// **Notes**
-/// - The Mooney-Rivlin model reduces to the [Neo-Hookean model](NeoHookean) when $`\mu_m\to 0`$.
+#[doc = include_str!("model.md")]
 #[derive(Debug)]
 pub struct MooneyRivlin<'a> {
     parameters: Parameters<'a>,
@@ -49,11 +32,7 @@ impl<'a> Solid<'a> for MooneyRivlin<'a> {
 }
 
 impl<'a> Elastic<'a> for MooneyRivlin<'a> {
-    /// Calculates and returns the Cauchy stress.
-    ///
-    /// ```math
-    /// \boldsymbol{\sigma}(\mathbf{F}) = \frac{\mu - \mu_m}{J}\, {\mathbf{B}^* }' - \frac{\mu_m}{J}\left(\mathbf{B}^{* -1}\right)' + \frac{\kappa}{2}\left(J - \frac{1}{J}\right)\mathbf{1}
-    /// ```
+    #[doc = include_str!("cauchy_stress.md")]
     fn calculate_cauchy_stress(
         &self,
         deformation_gradient: &DeformationGradient,
@@ -79,11 +58,7 @@ impl<'a> Elastic<'a> for MooneyRivlin<'a> {
             ))
         }
     }
-    /// Calculates and returns the tangent stiffness associated with the Cauchy stress.
-    ///
-    /// ```math
-    /// \mathcal{T}_{ijkL}(\mathbf{F}) = \frac{\mu-\mu_m}{J^{5/3}}\left(\delta_{ik}F_{jL} + \delta_{jk}F_{iL} - \frac{2}{3}\,\delta_{ij}F_{kL}- \frac{5}{3} \, B_{ij}'F_{kL}^{-T} \right) - \frac{\mu_m}{J}\left[ \frac{2}{3}\,B_{ij}^{* -1}F_{kL}^{-T} - B_{ik}^{* -1}F_{jL}^{-T} - B_{ik}^{* -1}F_{iL}^{-T} + \frac{2}{3}\,\delta_{ij}\left(B_{km}^{* -1}\right)'F_{mL}^{-T} - \left(B_{ij}^{* -1}\right)'F_{kL}^{-T} \right] + \frac{\kappa}{2} \left(J + \frac{1}{J}\right)\delta_{ij}F_{kL}^{-T}
-    /// ```
+    #[doc = include_str!("cauchy_tangent_stiffness.md")]
     fn calculate_cauchy_tangent_stiffness(
         &self,
         deformation_gradient: &DeformationGradient,
@@ -147,11 +122,7 @@ impl<'a> Elastic<'a> for MooneyRivlin<'a> {
 }
 
 impl<'a> Hyperelastic<'a> for MooneyRivlin<'a> {
-    /// Calculates and returns the Helmholtz free energy density.
-    ///
-    /// ```math
-    /// a(\mathbf{F}) = \frac{\mu - \mu_m}{2}\left[\mathrm{tr}(\mathbf{B}^* ) - 3\right] + \frac{\mu_m}{2}\left[I_2(\mathbf{B}^*) - 3\right] + \frac{\kappa}{2}\left[\frac{1}{2}\left(J^2 - 1\right) - \ln J\right]
-    /// ```
+    #[doc = include_str!("helmholtz_free_energy_density.md")]
     fn calculate_helmholtz_free_energy_density(
         &self,
         deformation_gradient: &DeformationGradient,
---
+++ src/constitutive/solid/hyperelastic/mooney_rivlin/model.md
@@ -0,0 +1,18 @@
+The Mooney-Rivlin hyperelastic constitutive model.[^mooney1940]<sup>,</sup>[^rivlin1948]
+
+[^mooney1940]: M. Mooney, [J. Appl. Phys. **11**, 582 (1940)](https://doi.org/10.1063/1.1712836).
+[^rivlin1948]: R.S. Rivlin, [Philos. Trans. R. Soc. London, Ser. A **241**, 379 (1948)](https://doi.org/10.1098/rsta.1948.0024).
+
+**Parameters**
+- The bulk modulus $`\kappa`$.
+- The shear modulus $`\mu`$.
+- The extra modulus $`\mu_m`$.
+
+**External variables**
+- The deformation gradient $`\mathbf{F}`$.
+
+**Internal variables**
+- None.
+
+**Notes**
+- The Mooney-Rivlin model reduces to the [Neo-Hookean model](NeoHookean) when $`\mu_m\to 0`$.
