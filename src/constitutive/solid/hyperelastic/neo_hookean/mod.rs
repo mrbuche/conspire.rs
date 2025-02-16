dcec4339b437862264548f56f1d115dd100ae88c
mrbuche <dev@mrbuche.com>
2025-02-16 15:21:07 -0700

    separate to use in book

--- src/constitutive/solid/hyperelastic/neo_hookean/mod.rs
+++ src/constitutive/solid/hyperelastic/neo_hookean/mod.rs
@@ -3,19 +3,7 @@ mod test;
 
 use super::*;
 
-/// The Neo-Hookean hyperelastic constitutive model.[^cite]
-///
-/// [^cite]: R.S. Rivlin, [Philos. Trans. R. Soc. London, Ser. A **240**, 459 (1948)](https://doi.org/10.1098/rsta.1948.0002).
-///
-/// **Parameters**
-/// - The bulk modulus $`\kappa`$.
-/// - The shear modulus $`\mu`$.
-///
-/// **External variables**
-/// - The deformation gradient $`\mathbf{F}`$.
-///
-/// **Internal variables**
-/// - None.
+#[doc = include_str!("model.md")]
 #[derive(Debug)]
 pub struct NeoHookean<'a> {
     parameters: Parameters<'a>,
---
+++ src/constitutive/solid/hyperelastic/neo_hookean/model.md
@@ -0,0 +1,13 @@
+The Neo-Hookean hyperelastic constitutive model.[^cite]
+
+[^cite]: R.S. Rivlin, [Philos. Trans. R. Soc. London, Ser. A **240**, 459 (1948)](https://doi.org/10.1098/rsta.1948.0002).
+
+**Parameters**
+- The bulk modulus $`\kappa`$.
+- The shear modulus $`\mu`$.
+
+**External variables**
+- The deformation gradient $`\mathbf{F}`$.
+
+**Internal variables**
+- None.
