d0c00f309f59385a03b136d2712a6a20f263ad94
mrbuche <dev@mrbuche.com>
2025-03-13 13:39:21 -0600

    welp

--- src/math/special/mod.rs
+++ src/math/special/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod test;
 
+use crate::{ABS_TOL, REL_TOL};
+use std::f64::consts::E;
+
 const NINE_FIFTHS: f64 = 9.0 / 5.0;
 
 /// Returns the inverse Langevin function.
@@ -45,6 +48,37 @@ pub fn inverse_langevin_approximate(y: f64) -> f64 {
     (2.14234 * y.powi(3) - 4.22785 * y.powi(2) + 3.0 * y)
         / (1.0 - y)
         / (0.71716 * y.powi(3) - 0.41103 * y.powi(2) - 0.39165 * y + 1.0)
+}
+
+/// Returns the Lambert W function.
+///
+/// ```math
+/// y = W_0(x)
+/// ```
+pub fn lambert_w(x: f64) -> f64 {
+    if x == -1.0 / E {
+        -1.0
+    } else if x == 0.0 {
+        0.0
+    } else if x == E {
+        1.0
+    } else if x < -1.0 / E {
+        panic!()
+    } else {
+        let mut w = if x < 0.0 {
+            (E * x * (1.0 + (1.0 + E * x).sqrt()).ln()) / (1.0 + E * x + (1.0 + E * x).sqrt())
+        } else if x < E {
+            x / E
+        } else {
+            x.ln() - x.ln().ln()
+        };
+        let mut error = w * w.exp() - x;
+        while error.abs() >= ABS_TOL && (error / x).abs() >= REL_TOL {
+            w *= (1.0 + (x / w).ln()) / (1.0 + w);
+            error = w * w.exp() - x;
+        }
+        w
+    }
 }
 
 /// Returns the Langevin function.
--- src/math/special/test.rs
+++ src/math/special/test.rs
@@ -1,15 +1,7 @@
-use super::{inverse_langevin, langevin};
-use crate::math::test::{assert_eq_within_tols, TestError};
+use super::{inverse_langevin, lambert_w, langevin, E};
+use crate::math::test::{assert_eq, assert_eq_within_tols, TestError};
 
 const LENGTH: usize = 10_000;
-
-mod langevin {
-    use super::*;
-    #[test]
-    fn zero() {
-        assert_eq!(langevin(0.0), 0.0)
-    }
-}
 
 mod inverse_langevin {
     use super::*;
@@ -33,7 +25,43 @@ mod inverse_langevin {
         })
     }
     #[test]
-    fn zero() {
-        assert_eq!(inverse_langevin(0.0), 0.0)
+    fn zero() -> Result<(), TestError> {
+        assert_eq(&inverse_langevin(0.0), &0.0)
+    }
+}
+
+mod lambert_w {
+    use super::*;
+    #[test]
+    fn end() -> Result<(), TestError> {
+        assert_eq(&lambert_w(-1.0 / E), &-1.0)
+    }
+    #[test]
+    fn euler() -> Result<(), TestError> {
+        assert_eq(&1.0_f64.exp(), &E)?;
+        assert_eq(&lambert_w(E), &1.0)
+    }
+    #[test]
+    fn range() -> Result<(), TestError> {
+        let mut x = -1.0 / E;
+        let dx = (6.0 - x) / ((LENGTH + 1) as f64);
+        let mut w = 0.0;
+        (0..LENGTH).try_for_each(|_| {
+            x += dx;
+            w = lambert_w(x);
+            assert_eq_within_tols(&(w * w.exp()), &x)
+        })
+    }
+    #[test]
+    fn zero() -> Result<(), TestError> {
+        assert_eq(&lambert_w(0.0), &0.0)
+    }
+}
+
+mod langevin {
+    use super::*;
+    #[test]
+    fn zero() -> Result<(), TestError> {
+        assert_eq(&langevin(0.0), &0.0)
     }
 }
