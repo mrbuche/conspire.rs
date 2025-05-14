7b4a52d0ae6a3de467696c43f07cbd76719a02d6
mrbuche <dev@mrbuche.com>
2025-05-14 14:03:33 -0600

    10 seems arbitrary

--- src/math/optimize/newton_raphson/mod.rs
+++ src/math/optimize/newton_raphson/mod.rs
@@ -52,7 +52,7 @@ where
                 let num_variables = initial_guess.num_entries();
                 let num_constraints = constraint_rhs.len();
                 let num_total = num_variables + num_constraints;
-                let mut multipliers = Vector::ones(num_constraints) * 10.0 * self.abs_tol;
+                let mut multipliers = Vector::ones(num_constraints) * self.abs_tol;
                 let mut residual = Vector::zero(num_total);
                 let mut solution = initial_guess;
                 let mut tangent = SquareMatrix::zero(num_total);
@@ -134,7 +134,7 @@ where
                 let num_variables = initial_guess.num_entries();
                 let num_constraints = constraint_rhs.len();
                 let num_total = num_variables + num_constraints;
-                let mut multipliers = Vector::ones(num_constraints) * 10.0 * self.abs_tol;
+                let mut multipliers = Vector::ones(num_constraints) * self.abs_tol;
                 let mut residual = Vector::zero(num_total);
                 let mut solution = initial_guess;
                 let mut tangent = SquareMatrix::zero(num_total);
