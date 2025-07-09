7701fd3f173e201ce95a6a429d5818546cd745b3
Michael R. Buche <mrbuche@sandia.gov>
2025-07-09 12:12:42 -0600

    this might work

--- src/constitutive/solid/elastic/doc.rs
+++ src/constitutive/solid/elastic/doc.rs
@@ -13,3 +13,19 @@ pub const ALMANSI_HAMEL: [[&str; 2]; 3] = [
         include_str!("almansi_hamel/cauchy_tangent_stiffness.md"),
     ],
 ];
+pub fn almansi_hamel<'a>() -> Vec<[&'a str; 2]> {
+    vec![
+        [
+            "src/constitutive/solid/elastic/almansi_hamel",
+            include_str!("almansi_hamel/doc.md"),
+        ],
+        [
+            "cauchy_stress",
+            include_str!("almansi_hamel/cauchy_stress.md"),
+        ],
+        [
+            "cauchy_tangent_stiffness",
+            include_str!("almansi_hamel/cauchy_tangent_stiffness.md"),
+        ],
+    ]
+}
