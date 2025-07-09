5542071011942ca0ebc52c0d124cf4445c9ec841
Michael R. Buche <mrbuche@sandia.gov>
2025-07-09 12:05:58 -0600

    this might work

--- src/constitutive/solid/elastic/doc.rs
+++ src/constitutive/solid/elastic/doc.rs
@@ -1,6 +1,9 @@
 pub const DOC: &str = include_str!("doc.md");
-pub const ALMANSI_HAMEL: [[&str; 2]; 3] = [
-    ["doc", include_str!("almansi_hamel/doc.md")],
+pub const ALMANSI_HAMEL: ([&str; 2], [&str; 2], [&str; 2]) = (
+    [
+        "src/constitutive/solid/elastic/almansi_hamel",
+        include_str!("almansi_hamel/doc.md"),
+    ],
     [
         "cauchy_stress",
         include_str!("almansi_hamel/cauchy_stress.md"),
@@ -9,4 +12,4 @@ pub const ALMANSI_HAMEL: [[&str; 2]; 3] = [
         "cauchy_tangent_stiffness",
         include_str!("almansi_hamel/cauchy_tangent_stiffness.md"),
     ],
-];
+);
