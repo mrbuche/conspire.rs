d9e7a16aa4f0282901abcd41ee715d2a7e5d1c56
Michael R. Buche <mrbuche@sandia.gov>
2025-08-13 14:10:46 -0600

    clippy

--- src/fem/block/element/linear/triangle/test.rs
+++ src/fem/block/element/linear/triangle/test.rs
@@ -166,7 +166,7 @@ fn equality_constraint() -> (
 
 fn applied_velocity(
     times: &crate::math::Vector,
-) -> crate::constitutive::solid::viscoelastic::AppliedLoad<'_> {
+) -> crate::constitutive::solid::viscoelastic::AppliedLoad {
     crate::constitutive::solid::viscoelastic::AppliedLoad::BiaxialStress(
         |_| 0.23,
         |_| 0.0,
