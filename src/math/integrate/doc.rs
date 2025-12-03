f4708a80d589896c0df988925a90c46453d3a927
Michael R. Buche <mrbuche@sandia.gov>
2025-12-03 14:49:09 -0700

    doc

--- src/math/integrate/doc.rs
+++ src/math/integrate/doc.rs
@@ -3,29 +3,35 @@ pub const IMPLICIT: &str = include_str!("implicit.md");
 
 pub fn bogacki_shampine<'a>() -> Vec<[&'a str; 2]> {
     vec![[
-        "math/integrate/bogacki_shampine",
+        "math/integrate/explicit/bogacki_shampine",
         include_str!("bogacki_shampine/doc.md"),
     ]]
 }
 
 pub fn dormand_prince<'a>() -> Vec<[&'a str; 2]> {
     vec![[
-        "math/integrate/dormand_prince",
+        "math/integrate/explicit/dormand_prince",
         include_str!("dormand_prince/doc.md"),
     ]]
 }
 
 pub fn verner_8<'a>() -> Vec<[&'a str; 2]> {
-    vec![["math/integrate/verner_8", include_str!("verner_8/doc.md")]]
+    vec![[
+        "math/integrate/explicit/verner_8",
+        include_str!("verner_8/doc.md"),
+    ]]
 }
 
 pub fn verner_9<'a>() -> Vec<[&'a str; 2]> {
-    vec![["math/integrate/verner_9", include_str!("verner_9/doc.md")]]
+    vec![[
+        "math/integrate/explicit/verner_9",
+        include_str!("verner_9/doc.md"),
+    ]]
 }
 
 pub fn backward_euler<'a>() -> Vec<[&'a str; 2]> {
     vec![[
-        "math/integrate/backward_euler",
+        "math/integrate/implicit/backward_euler",
         include_str!("backward_euler/doc.md"),
     ]]
 }
