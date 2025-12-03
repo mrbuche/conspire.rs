f0c65302c33b30af1ddcab4c76c9180a38747ce5
Michael R. Buche <mrbuche@sandia.gov>
2025-12-03 13:24:24 -0700

    doc

--- src/math/integrate/doc.rs
+++ src/math/integrate/doc.rs
@@ -4,3 +4,18 @@ pub fn bogacki_shampine<'a>() -> Vec<[&'a str; 2]> {
         include_str!("bogacki_shampine/doc.md"),
     ]]
 }
+
+pub fn dormand_prince<'a>() -> Vec<[&'a str; 2]> {
+    vec![[
+        "math/integrate/dormand_prince",
+        include_str!("dormand_prince/doc.md"),
+    ]]
+}
+
+pub fn verner_8<'a>() -> Vec<[&'a str; 2]> {
+    vec![["math/integrate/verner_8", include_str!("verner_8/doc.md")]]
+}
+
+pub fn verner_9<'a>() -> Vec<[&'a str; 2]> {
+    vec![["math/integrate/verner_9", include_str!("verner_9/doc.md")]]
+}
