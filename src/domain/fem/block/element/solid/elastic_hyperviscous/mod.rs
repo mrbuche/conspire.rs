8ace89bfd6fb08535a797ac1c0b86341d16ea7f8
mrbuche <dev@mrbuche.com>
2025-12-19 11:51:15 -0700

    extreme templating

--- src/domain/fem/block/element/solid/elastic_hyperviscous/mod.rs
+++ src/domain/fem/block/element/solid/elastic_hyperviscous/mod.rs
@@ -1,10 +1,8 @@
 use crate::{
     constitutive::{ConstitutiveError, solid::elastic_hyperviscous::ElasticHyperviscous},
     fem::block::element::{
-        Element, ElementNodalCoordinates, ElementNodalVelocities, FiniteElement,
-        FiniteElementError,
-        solid::{SolidFiniteElement, viscoelastic::ViscoelasticFiniteElement},
-        surface::SurfaceElement,
+        Element, ElementNodalCoordinates, ElementNodalVelocities, FiniteElementError,
+        solid::viscoelastic::ViscoelasticFiniteElement, surface::SurfaceElement,
     },
     math::{Scalar, Tensor},
 };
@@ -40,31 +38,12 @@ where
         nodal_coordinates: &ElementNodalCoordinates<N>,
         nodal_velocities: &ElementNodalVelocities<N>,
     ) -> Result<Scalar, FiniteElementError> {
-        match self
-            .deformation_gradients(nodal_coordinates)
-            .iter()
-            .zip(
-                self.deformation_gradient_rates(nodal_coordinates, nodal_velocities)
-                    .iter()
-                    .zip(self.integration_weights()),
-            )
-            .map(
-                |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
-                    Ok::<_, ConstitutiveError>(
-                        constitutive_model
-                            .viscous_dissipation(deformation_gradient, deformation_gradient_rate)?
-                            * integration_weight,
-                    )
-                },
-            )
-            .sum()
-        {
-            Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
-            Err(error) => Err(FiniteElementError::Upstream(
-                format!("{error}"),
-                format!("{self:?}"),
-            )),
-        }
+        viscous_dissipation::<_, _, _, _, _, O>(
+            self,
+            constitutive_model,
+            nodal_coordinates,
+            nodal_velocities,
+        )
     }
     fn dissipation_potential(
         &self,
@@ -72,32 +51,12 @@ where
         nodal_coordinates: &ElementNodalCoordinates<N>,
         nodal_velocities: &ElementNodalVelocities<N>,
     ) -> Result<Scalar, FiniteElementError> {
-        match self
-            .deformation_gradients(nodal_coordinates)
-            .iter()
-            .zip(
-                self.deformation_gradient_rates(nodal_coordinates, nodal_velocities)
-                    .iter()
-                    .zip(self.integration_weights()),
-            )
-            .map(
-                |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
-                    Ok::<_, ConstitutiveError>(
-                        constitutive_model.dissipation_potential(
-                            deformation_gradient,
-                            deformation_gradient_rate,
-                        )? * integration_weight,
-                    )
-                },
-            )
-            .sum()
-        {
-            Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
-            Err(error) => Err(FiniteElementError::Upstream(
-                format!("{error}"),
-                format!("{self:?}"),
-            )),
-        }
+        dissipation_potential::<_, _, _, _, _, O>(
+            self,
+            constitutive_model,
+            nodal_coordinates,
+            nodal_velocities,
+        )
     }
 }
 
@@ -113,31 +72,12 @@ where
         nodal_coordinates: &ElementNodalCoordinates<N>,
         nodal_velocities: &ElementNodalVelocities<N>,
     ) -> Result<Scalar, FiniteElementError> {
-        match self
-            .deformation_gradients(nodal_coordinates)
-            .iter()
-            .zip(
-                self.deformation_gradient_rates(nodal_coordinates, nodal_velocities)
-                    .iter()
-                    .zip(self.integration_weights()),
-            )
-            .map(
-                |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
-                    Ok::<_, ConstitutiveError>(
-                        constitutive_model
-                            .viscous_dissipation(deformation_gradient, deformation_gradient_rate)?
-                            * integration_weight,
-                    )
-                },
-            )
-            .sum()
-        {
-            Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
-            Err(error) => Err(FiniteElementError::Upstream(
-                format!("{error}"),
-                format!("{self:?}"),
-            )),
-        }
+        viscous_dissipation::<_, _, _, _, _, O>(
+            self,
+            constitutive_model,
+            nodal_coordinates,
+            nodal_velocities,
+        )
     }
     fn dissipation_potential(
         &self,
@@ -145,31 +85,87 @@ where
         nodal_coordinates: &ElementNodalCoordinates<N>,
         nodal_velocities: &ElementNodalVelocities<N>,
     ) -> Result<Scalar, FiniteElementError> {
-        match self
-            .deformation_gradients(nodal_coordinates)
-            .iter()
-            .zip(
-                self.deformation_gradient_rates(nodal_coordinates, nodal_velocities)
-                    .iter()
-                    .zip(self.integration_weights()),
-            )
-            .map(
-                |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
-                    Ok::<_, ConstitutiveError>(
-                        constitutive_model.dissipation_potential(
-                            deformation_gradient,
-                            deformation_gradient_rate,
-                        )? * integration_weight,
-                    )
-                },
-            )
-            .sum()
-        {
-            Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
-            Err(error) => Err(FiniteElementError::Upstream(
-                format!("{error}"),
-                format!("{self:?}"),
-            )),
-        }
+        dissipation_potential::<_, _, _, _, _, O>(
+            self,
+            constitutive_model,
+            nodal_coordinates,
+            nodal_velocities,
+        )
+    }
+}
+
+fn viscous_dissipation<C, F, const G: usize, const M: usize, const N: usize, const O: usize>(
+    finite_element: &F,
+    constitutive_model: &C,
+    nodal_coordinates: &ElementNodalCoordinates<N>,
+    nodal_velocities: &ElementNodalVelocities<N>,
+) -> Result<Scalar, FiniteElementError>
+where
+    C: ElasticHyperviscous,
+    F: ViscoelasticFiniteElement<C, G, M, N>,
+{
+    match finite_element
+        .deformation_gradients(nodal_coordinates)
+        .iter()
+        .zip(
+            finite_element
+                .deformation_gradient_rates(nodal_coordinates, nodal_velocities)
+                .iter()
+                .zip(finite_element.integration_weights()),
+        )
+        .map(
+            |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
+                Ok::<_, ConstitutiveError>(
+                    constitutive_model
+                        .viscous_dissipation(deformation_gradient, deformation_gradient_rate)?
+                        * integration_weight,
+                )
+            },
+        )
+        .sum()
+    {
+        Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
+        Err(error) => Err(FiniteElementError::Upstream(
+            format!("{error}"),
+            format!("{finite_element:?}"),
+        )),
+    }
+}
+
+fn dissipation_potential<C, F, const G: usize, const M: usize, const N: usize, const O: usize>(
+    finite_element: &F,
+    constitutive_model: &C,
+    nodal_coordinates: &ElementNodalCoordinates<N>,
+    nodal_velocities: &ElementNodalVelocities<N>,
+) -> Result<Scalar, FiniteElementError>
+where
+    C: ElasticHyperviscous,
+    F: ViscoelasticFiniteElement<C, G, M, N>,
+{
+    match finite_element
+        .deformation_gradients(nodal_coordinates)
+        .iter()
+        .zip(
+            finite_element
+                .deformation_gradient_rates(nodal_coordinates, nodal_velocities)
+                .iter()
+                .zip(finite_element.integration_weights()),
+        )
+        .map(
+            |(deformation_gradient, (deformation_gradient_rate, integration_weight))| {
+                Ok::<_, ConstitutiveError>(
+                    constitutive_model
+                        .dissipation_potential(deformation_gradient, deformation_gradient_rate)?
+                        * integration_weight,
+                )
+            },
+        )
+        .sum()
+    {
+        Ok(helmholtz_free_energy) => Ok(helmholtz_free_energy),
+        Err(error) => Err(FiniteElementError::Upstream(
+            format!("{error}"),
+            format!("{finite_element:?}"),
+        )),
     }
 }
