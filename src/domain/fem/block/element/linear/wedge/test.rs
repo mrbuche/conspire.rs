868194e3ab61c1464766f35beda56b827c90bf5d
mrbuche <dev@mrbuche.com>
2025-12-23 09:56:29 -0700

    yerp

--- src/domain/fem/block/element/linear/wedge/test.rs
+++ src/domain/fem/block/element/linear/wedge/test.rs
@@ -1,42 +1,66 @@
 use crate::{
-    fem::block::element::{
-        ElementNodalCoordinates, ElementNodalReferenceCoordinates, ElementNodalVelocities,
-        GradientVectors,
-        linear::wedge::{G, N, Wedge},
-        solid::SolidFiniteElement,
-        test::test_finite_element,
+    fem::{
+        NodalCoordinates, NodalReferenceCoordinates, NodalVelocities,
+        block::{
+            Block, Connectivity, FiniteElementBlock,
+            element::{
+                ElementNodalCoordinates, ElementNodalReferenceCoordinates, ElementNodalVelocities,
+                GradientVectors,
+                linear::wedge::{G, N, Wedge},
+                solid::SolidFiniteElement,
+                test::test_finite_element,
+            },
+            solid::{NodalForcesSolid, NodalStiffnessesSolid},
+            test::test_finite_element_block,
+        },
     },
-    math::{ScalarList, Tensor},
+    math::{ScalarList, Tensor, optimize::EqualityConstraint},
     mechanics::{DeformationGradient, DeformationGradientList, DeformationGradientRateList},
 };
 
-const D: usize = 14;
+const D: usize = 21;
 
 fn get_connectivity() -> Connectivity<N> {
-    // vec![
-    //     [4, 5, 1, 0, 8],
-    //     [5, 6, 2, 1, 8],
-    //     [6, 7, 3, 2, 8],
-    //     [0, 3, 7, 4, 8],
-    //     [0, 1, 2, 3, 8],
-    //     [5, 4, 7, 6, 8],
-    // ]
-    todo!()
+    vec![
+        [6, 0, 1, 13, 7, 8],
+        [6, 5, 0, 13, 12, 7],
+        [6, 4, 5, 13, 11, 12],
+        [6, 3, 4, 13, 10, 11],
+        [6, 2, 3, 13, 9, 10],
+        [6, 1, 2, 13, 8, 9],
+        [13, 7, 8, 20, 14, 15],
+        [13, 12, 7, 20, 19, 14],
+        [13, 11, 12, 20, 18, 19],
+        [13, 10, 11, 20, 17, 18],
+        [13, 9, 10, 20, 16, 17],
+        [13, 8, 9, 20, 15, 16],
+    ]
 }
 
 fn get_coordinates_block() -> NodalCoordinates {
-    // NodalCoordinates::from([
-    //     [0.04175951, 0.00963520, -0.08547185],
-    //     [1.08264022, 0.06657146, -0.06028449],
-    //     [1.03545020, 0.95664729, 0.02444034],
-    //     [0.03195872, 0.91151568, 0.01357932],
-    //     [0.05957727, 0.09722483, 0.95352398],
-    //     [1.09602809, 0.05991935, 0.92856463],
-    //     [1.00712265, 0.99487330, 0.97093928],
-    //     [0.03305756, 1.06846662, 1.02871468],
-    //     [0.55951995, 0.55421498, 0.56169451],
-    // ])
-    todo!()
+    NodalCoordinates::from([
+        [-0.09878799, -2.04153736, 0.04151086],
+        [1.63754285, -0.99562875, -0.09887788],
+        [1.78325268, 0.91336429, 0.0046213],
+        [0.0315871, 1.98668723, -0.03387963],
+        [-1.7616809, 1.05434027, -0.06391626],
+        [-1.83054934, -0.918871, -0.06934134],
+        [-0.04606216, 0.05635717, -0.06762617],
+        [0.06188456, -1.94417605, 0.94833794],
+        [1.77811662, -1.06366191, 0.99248902],
+        [1.83070253, 0.90495846, 0.90698956],
+        [0.04247115, 1.93735475, 0.94649422],
+        [-1.68616882, 0.93665319, 0.98529049],
+        [-1.79665063, -1.07254509, 1.01704746],
+        [-0.06952031, -0.09988782, 1.02782312],
+        [0.02586393, -2.00549432, 2.06038998],
+        [1.66721894, -1.00169116, 1.92066057],
+        [1.69052491, 0.96102527, 1.95475384],
+        [-0.05315483, 2.0087481, 1.93177288],
+        [-1.72731234, 0.90184272, 1.94313904],
+        [-1.77668581, -1.01445848, 1.95960534],
+        [-0.01508426, -0.09189985, 2.00903682],
+    ])
 }
 
 fn reference_coordinates() -> ElementNodalReferenceCoordinates<N> {
@@ -52,22 +76,66 @@ fn reference_coordinates() -> ElementNodalReferenceCoordinates<N> {
 
 fn get_reference_coordinates_block() -> NodalReferenceCoordinates {
     NodalReferenceCoordinates::from([
-        [0.0, -1.0, 0.0],
-        [3.0.sqrt() / 2.0, -1.0 / 2.0, 0.0],
-        [3.0.sqrt() / 2.0, 1.0 / 2.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [-3.0.sqrt() / 2.0, 1.0 / 2.0, 0.0],
-        [-3.0.sqrt() / 2.0, -1.0 / 2.0, 0.0],
+        [0.0, -2.0, 0.0],
+        [3.0_f64.sqrt(), -1.0, 0.0],
+        [3.0_f64.sqrt(), 1.0, 0.0],
+        [0.0, 2.0, 0.0],
+        [-3.0_f64.sqrt(), 1.0, 0.0],
+        [-3.0_f64.sqrt(), -1.0, 0.0],
         [0.0, 0.0, 0.0],
-
-        [0.0, -1.0, 1.0],
-        [3.0.sqrt() / 2.0, -1.0 / 2.0, 1.0],
-        [3.0.sqrt() / 2.0, 1.0 / 2.0, 1.0],
-        [0.0, 1.0, 1.0],
-        [-3.0.sqrt() / 2.0, 1.0 / 2.0, 1.0],
-        [-3.0.sqrt() / 2.0, -1.0 / 2.0, 1.0],
+        [0.0, -2.0, 1.0],
+        [3.0_f64.sqrt(), -1.0, 1.0],
+        [3.0_f64.sqrt(), 1.0, 1.0],
+        [0.0, 2.0, 1.0],
+        [-3.0_f64.sqrt(), 1.0, 1.0],
+        [-3.0_f64.sqrt(), -1.0, 1.0],
         [0.0, 0.0, 1.0],
+        [0.0, -2.0, 2.0],
+        [3.0_f64.sqrt(), -1.0, 2.0],
+        [3.0_f64.sqrt(), 1.0, 2.0],
+        [0.0, 2.0, 2.0],
+        [-3.0_f64.sqrt(), 1.0, 2.0],
+        [-3.0_f64.sqrt(), -1.0, 2.0],
+        [0.0, 0.0, 2.0],
     ])
 }
 
+fn get_velocities_block() -> NodalVelocities {
+    NodalVelocities::from([
+        [-0.09749712, -0.0025994, -0.06657566],
+        [0.09572879, -0.05925453, 0.07497072],
+        [-0.0683048, 0.0984304, 0.0454972],
+        [-0.02008789, 0.07622423, -0.09598549],
+        [0.06569026, -0.07759665, -0.05319748],
+        [-0.0340313, -0.01151086, 0.08998172],
+        [0.06716707, 0.03681826, 0.04703527],
+        [-0.00529704, -0.00629051, 0.06642474],
+        [0.05937476, -0.01342672, 0.04942817],
+        [-0.04740402, 0.09363964, 0.04930639],
+        [-0.05330261, -0.02076028, 0.05441078],
+        [0.0685281, 0.01355718, 0.02223258],
+        [0.06832807, 0.03535636, 0.03274881],
+        [0.0686753, -0.02731762, 0.03348459],
+    ])
+}
+
+fn equality_constraint() -> (
+    crate::constitutive::solid::elastic::AppliedLoad,
+    crate::math::Matrix,
+    crate::math::Vector,
+) {
+    todo!()
+}
+
+fn applied_velocity(
+    times: &crate::math::Vector,
+) -> crate::constitutive::solid::viscoelastic::AppliedLoad<'_> {
+    todo!()
+}
+
+fn applied_velocities() -> (crate::math::Matrix, crate::math::Vector) {
+    todo!()
+}
+
 test_finite_element!(Wedge);
+test_finite_element_block!(Wedge);
