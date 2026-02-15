cbd44dcf0f51cdb9626dedd9aaf2f44c8ccbdac3
mrbuche <dev@mrbuche.com>
2026-02-15 10:07:37 -0700

    should be right, a few clones but whatever

--- src/constitutive/hybrid/viscoplastic/additive/viscoplastic/mod.rs
+++ src/constitutive/hybrid/viscoplastic/additive/viscoplastic/mod.rs
@@ -50,8 +50,7 @@ where
         state_variables: &ViscoplasticStateVariables<GroupedViscoplasticStateVariables<Y1, Y2>>,
     ) -> Result<NestedViscoplasticStateVariables<Y1, Y2>, ConstitutiveError> {
         let state_variables_1 = &state_variables.1.0;
-        let state_variables_2 =
-            &TensorTuple::from((state_variables.0.clone(), state_variables.1.1.clone())); // would be nice to avoid theses clones somehow
+        let state_variables_2 = &(state_variables.0.clone(), state_variables.1.1.clone()).into();
         let deformation_gradient = (&state_variables.0).into();
         let deformation_gradient_p = &state_variables_1.0;
         let jacobian = self.0.jacobian(deformation_gradient)?;
@@ -60,16 +59,16 @@ where
             .0
             .cauchy_stress(deformation_gradient, deformation_gradient_p)?;
         let mandel_stress_1 = (deformation_gradient_e.transpose()
-            * cauchy_stress_1
+            * &cauchy_stress_1
             * deformation_gradient_e.inverse_transpose())
             * jacobian;
-        let mandel_stress_2 = mandel_stress - &mandel_stress_1; // incorrect, need to pass in Fe (and at that point, maybe pass in cauchy instead of mandel)
+        let cauchy_stress_2 = mandel_stress - MandelStressElastic::from(cauchy_stress_1);
         let evolution_1 = self
             .0
             .plastic_evolution(mandel_stress_1, state_variables_1)?;
         let (deformation_gradient_rate, dydt_2) = self
             .1
-            .plastic_evolution(mandel_stress_2, state_variables_2)?
+            .plastic_evolution(cauchy_stress_2, state_variables_2)?
             .into();
         Ok((deformation_gradient_rate, (evolution_1, dydt_2).into()).into())
     }
