7821b08da2204083261b237f688fe150d8e2d14d
Michael R. Buche <mrbuche@sandia.gov>
2026-02-18 15:58:31 -0700

    quad sj

--- src/domain/fem/block/element/surface/linear/quadrilateral/mod.rs
+++ src/domain/fem/block/element/surface/linear/quadrilateral/mod.rs
@@ -7,12 +7,15 @@ use crate::{
         ParametricCoordinates, ParametricReference, ShapeFunctions, ShapeFunctionsGradients,
         surface::{M, linear::LinearSurfaceElement},
     },
-    math::ScalarList,
+    math::{ScalarList, Tensor, TensorArray},
+    mechanics::Coordinate,
 };
 
 const G: usize = 4;
 const N: usize = 4;
 const P: usize = N;
+
+const CORNERS: [[usize; 2]; N] = [[1, 3], [2, 0], [3, 1], [0, 2]];
 
 pub type Quadrilateral = LinearSurfaceElement<G, N>;
 
@@ -36,9 +39,26 @@ impl FiniteElement<G, M, N, P> for Quadrilateral {
         [1.0; G].into()
     }
     fn scaled_jacobians<const I: usize>(
-        _nodal_coordinates: ElementNodalEitherCoordinates<I, N>,
+        nodal_coordinates: ElementNodalEitherCoordinates<I, N>,
     ) -> ScalarList<P> {
-        todo!()
+        let mut u = Coordinate::zero();
+        let mut v = Coordinate::zero();
+        let mut n = Coordinate::zero();
+        let x = (&nodal_coordinates[1] - &nodal_coordinates[0])
+            + (&nodal_coordinates[2] - &nodal_coordinates[3]);
+        let y = (&nodal_coordinates[2] - &nodal_coordinates[1])
+            + (&nodal_coordinates[3] - &nodal_coordinates[0]);
+        let nc = x.cross(&y).normalized();
+        CORNERS
+            .into_iter()
+            .enumerate()
+            .map(|(node, [node_a, node_b])| {
+                u = &nodal_coordinates[node_a] - &nodal_coordinates[node];
+                v = &nodal_coordinates[node_b] - &nodal_coordinates[node];
+                n = u.cross(&v);
+                (&n * &nc) / u.norm() / v.norm()
+            })
+            .collect()
     }
     fn shape_functions(parametric_coordinate: ParametricCoordinate<M>) -> ShapeFunctions<N> {
         let [xi_1, xi_2] = parametric_coordinate.into();
