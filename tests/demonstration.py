32e359576f4767ad175943ce7cafc53bd1c34b7b
mrbuche <dev@mrbuche.com>
2026-02-20 16:46:59 -0700

    maybe this is fine

--- tests/demonstration.py
+++ tests/demonstration.py
@@ -6,15 +6,13 @@ time = data[:, 0]
 strain = data[:, 1]
 stress = data[:, 2]
 
-plt.plot(time, strain, '.-')
+plt.plot(time, strain)
 plt.show()
 
 plt.plot(strain, stress)
-# plt.xlim([0.0, 0.6])
-# plt.ylim([0.0, 30.0])
 plt.show()
 
 for eqps in data[:, 3:].T:
-    plt.plot(strain, eqps)
+    plt.plot(time, eqps)
 
 plt.show()
