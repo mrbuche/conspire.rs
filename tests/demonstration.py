181a0251dee0092fc96912555395b96364e5c7da
Michael R. Buche <mrbuche@sandia.gov>
2026-02-20 15:40:16 -0700

    something like this

--- tests/demonstration.py
+++ tests/demonstration.py
@@ -6,7 +6,12 @@ time = data[:, 0]
 strain = data[:, 1]
 stress = data[:, 2]
 
+plt.plot(time, strain, '.-')
+plt.show()
+
 plt.plot(strain, stress)
+# plt.xlim([0.0, 0.6])
+# plt.ylim([0.0, 30.0])
 plt.show()
 
 for eqps in data[:, 3:].T:
--- tests/demonstration.rs
+++ tests/demonstration.rs
@@ -10,7 +10,7 @@ use conspire::{
         solid::{
             elastic_plastic::ElasticPlasticOrViscoplastic,
             elastic_viscoplastic::AppliedLoad,
-            hyperelastic::{ArrudaBoyce, Hencky},
+            hyperelastic::{Hencky, NeoHookean},
             hyperelastic_viscoplastic::SecondOrderMinimize,
         },
     },
@@ -22,33 +22,33 @@ fn demonstrate() -> Result<(), TestError> {
     let model = ElasticViscoplasticAdditiveElastic::from((
         ElasticMultiplicativeViscoplastic::from((
             Hencky {
-                bulk_modulus: 100.0,
-                shear_modulus: 10.0,
+                bulk_modulus: 300.0,
+                shear_modulus: 100.0,
             },
             ElasticViscoplasticAdditiveViscoplastic::from((
                 ElasticMultiplicativeViscoplastic::from((
                     Hencky {
                         bulk_modulus: 0.0,
-                        shear_modulus: 0.0,
+                        shear_modulus: 25.0,
                     },
                     ElasticViscoplasticAdditiveViscoplastic::from((
                         ElasticMultiplicativeViscoplastic::from((
                             Hencky {
                                 bulk_modulus: 0.0,
-                                shear_modulus: 0.0,
+                                shear_modulus: 10.0,
                             },
                             ViscoplasticFlow {
-                                yield_stress: 1.0,
-                                hardening_slope: 1.0,
+                                yield_stress: 3.0,
+                                hardening_slope: 0.0,
                                 rate_sensitivity: 0.25,
-                                reference_flow_rate: 0.001,
+                                reference_flow_rate: 0.04,
                             },
                         )),
                         ViscoplasticFlow {
-                            yield_stress: 1.0,
-                            hardening_slope: 1.0,
+                            yield_stress: 2.0,
+                            hardening_slope: 0.0,
                             rate_sensitivity: 0.25,
-                            reference_flow_rate: 0.001,
+                            reference_flow_rate: 0.02,
                         },
                     )),
                 )),
@@ -56,32 +56,50 @@ fn demonstrate() -> Result<(), TestError> {
                     yield_stress: 1.0,
                     hardening_slope: 1.0,
                     rate_sensitivity: 0.25,
-                    reference_flow_rate: 0.001,
+                    reference_flow_rate: 0.01,
                 },
             )),
         )),
-        ArrudaBoyce {
+        NeoHookean {
             bulk_modulus: 0.0,
-            shear_modulus: 1.0,
-            number_of_links: 23.0,
+            shear_modulus: 3.3,
         },
     ));
     let (t, f, f_p) = model.minimize(
         AppliedLoad::UniaxialStress(
             |t| {
-                if t < 1.0 {
+                if t < 0.25_f64.exp() - 1.0 {
                     1.0 + t
-                } else if t < 1.5 {
-                    2.0 - (t - 1.0)
-                } else if t < 2.5 {
-                    1.5 + (t - 1.5)
-                } else if t < 3.0 {
-                    2.5 - (t - 2.5)
+                } else if t < 0.25_f64.exp() - 1.0 + 0.13_f64.exp() - 1.0 {
+                    let t0 = 0.25_f64.exp() - 1.0;
+                    0.25_f64.exp() - (t - t0)
+                } else if t < 2.0 * (0.25_f64.exp() - 1.0) + 0.13_f64.exp() - 1.0 {
+                    let t0 = 0.25_f64.exp() - 1.0 + 0.13_f64.exp() - 1.0 - 1.0;
+                    0.25_f64.exp() - 0.13_f64.exp() + (t - t0)
+                } else if t < 2.0 * (0.25_f64.exp() - 1.0) + (0.13_f64.exp() - 1.0) + (0.18_f64.exp() - 1.0) {
+                    let t0 = 2.0 * (0.25_f64.exp() - 1.0) + 0.13_f64.exp() - 1.0;
+                    2.0 * 0.25_f64.exp() - 0.13_f64.exp() - (t - t0)
+                } else if t < 3.0 * (0.25_f64.exp() - 1.0) + (0.13_f64.exp() - 1.0) + (0.18_f64.exp() - 1.0) {
+                    let t0 = 2.0 * (0.25_f64.exp() - 1.0) + (0.13_f64.exp() - 1.0) + (0.18_f64.exp() - 1.0) - 1.0;
+                    2.0 * 0.25_f64.exp() - 0.13_f64.exp() - 0.18_f64.exp() + (t - t0)
                 } else {
-                    2.0 + (t - 3.0)
+                    let t0 = 3.0 * (0.25_f64.exp() - 1.0) + (0.13_f64.exp() - 1.0) + (0.18_f64.exp() - 1.0);
+                    3.0 * 0.25_f64.exp() - 0.13_f64.exp() - 0.18_f64.exp() - (t - t0)
                 }
+                // } else if t < 1.5 {
+                //     2.0 - (t - 1.0)
+                // } else if t < 2.5 {
+                //     1.5 + (t - 1.5)
+                // } else if t < 3.0 {
+                //     2.5 - (t - 2.5)
+                // } else {
+                //     2.0 + (t - 3.0)
+                // }
             },
-            &[0.0, 4.0],
+            &[
+                0.0,
+                3.0 * (0.25_f64.exp() - 1.0) + 3.0 * (0.13_f64.exp() - 1.0),
+            ],
         ),
         DormandPrince::default(),
         NewtonRaphson::default(),
@@ -90,7 +108,7 @@ fn demonstrate() -> Result<(), TestError> {
         let (f_p_i, others) = s_i.into();
         let (others, eqps) = others.into();
         let (_, others) = others.into();
-        let (_, eqps_1) = others.into();
+        let (others, eqps_1) = others.into();
         let (_, eqps_2) = others.into();
         let c_e = model.cauchy_stress(f_i, f_p_i)?;
         println!(
