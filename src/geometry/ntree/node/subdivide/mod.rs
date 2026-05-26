4e5a67546bf4808f2563e6556ec34ba74e151251
Michael R. Buche <mrbuche@sandia.gov>
2026-05-26 14:46:02 -0600

    starting over carefully

--- src/geometry/ntree/node/subdivide/mod.rs
+++ src/geometry/ntree/node/subdivide/mod.rs
@@ -1,140 +1,202 @@
 use crate::geometry::ntree::{
     error::OrthotreeError,
-    node::{Kind, Node, split::Split},
+    node::{split::Split, Kind, Node},
 };
 use std::{array::from_fn, ops::AddAssign};
 
-impl<T, U> Node<3, 6, 8, T, U>
+impl<const D: usize, const M: usize, const N: usize, T, U> Node<D, M, N, T, U>
 where
     T: AddAssign + Copy + Split,
     U: Copy,
 {
-    pub fn subdivide(&self, indices: [U; 8]) -> Result<[Self; 8], OrthotreeError> {
+    pub fn subdivide(&self, indices: [U; N]) -> Result<[Self; N], OrthotreeError> {
         match self.kind {
             Kind::Leaf => {
                 let length = self.length.split();
-                let min_x = self.corner[0];
-                let min_y = self.corner[1];
-                let min_z = self.corner[2];
-                let mut val_x = min_x;
-                val_x += length;
-                let mut val_y = min_y;
-                val_y += length;
-                let mut val_z = min_z;
-                val_z += length;
                 let corner = self.corner;
-                let facets = self.facets;
-                Ok([
+                let parent_facets = self.facets;
+
+                Ok(from_fn(|i| {
+                    let child_corner = from_fn(|axis| {
+                        if ((i >> axis) & 1) == 1 {
+                            let mut c = corner[axis];
+                            c += length;
+                            c
+                        } else {
+                            corner[axis]
+                        }
+                    });
+
+                    let child_facets = from_fn(|f| {
+                        let axis = f / 2;
+                        let is_plus_face = (f % 2) == 1;
+                        let child_on_high_side = ((i >> axis) & 1) == 1;
+
+                        match (child_on_high_side, is_plus_face) {
+                            // child is on low side:
+                            //   - face is inherited
+                            //   + face is internal
+                            (false, false) => parent_facets[f],
+                            (false, true) => Some(indices[i ^ (1 << axis)]),
+
+                            // child is on high side:
+                            //   - face is internal
+                            //   + face is inherited
+                            (true, false) => Some(indices[i ^ (1 << axis)]),
+                            (true, true) => parent_facets[f],
+                        }
+                    });
+
                     Node {
-                        corner: [min_x, min_y, min_z],
+                        corner: child_corner,
                         length,
-                        facets: [
-                            None,
-                            Some(indices[1]),
-                            None,
-                            Some(indices[2]),
-                            None,
-                            Some(indices[4]),
-                        ],
+                        facets: child_facets,
                         kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [val_x, min_y, min_z],
-                        length,
-                        facets: [
-                            Some(indices[0]),
-                            None,
-                            None,
-                            Some(indices[3]),
-                            None,
-                            Some(indices[5]),
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [min_x, val_y, min_z],
-                        length,
-                        facets: [
-                            None,
-                            Some(indices[3]),
-                            Some(indices[0]),
-                            None,
-                            None,
-                            Some(indices[6]),
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [val_x, val_y, min_z],
-                        length,
-                        facets: [
-                            Some(indices[2]),
-                            None,
-                            Some(indices[1]),
-                            None,
-                            None,
-                            Some(indices[7]),
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [min_x, min_y, val_z],
-                        length,
-                        facets: [
-                            None,
-                            Some(indices[5]),
-                            None,
-                            Some(indices[6]),
-                            Some(indices[0]),
-                            None,
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [val_x, min_y, val_z],
-                        length,
-                        facets: [
-                            Some(indices[4]),
-                            None,
-                            None,
-                            Some(indices[7]),
-                            Some(indices[1]),
-                            None,
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [min_x, val_y, val_z],
-                        length,
-                        facets: [
-                            None,
-                            Some(indices[7]),
-                            Some(indices[4]),
-                            None,
-                            Some(indices[2]),
-                            None,
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                    Node {
-                        corner: [val_x, val_y, val_z],
-                        length,
-                        facets: [
-                            Some(indices[6]),
-                            None,
-                            Some(indices[5]),
-                            None,
-                            Some(indices[3]),
-                            None,
-                        ],
-                        kind: Kind::Leaf,
-                    },
-                ])
+                    }
+                }))
             }
             Kind::Tree(_) => Err(OrthotreeError::CannotSubdivideTree),
         }
     }
 }
+
+// use crate::geometry::ntree::{
+//     error::OrthotreeError,
+//     node::{Kind, Node, split::Split},
+// };
+// use std::{array::from_fn, ops::AddAssign};
+
+// impl<T, U> Node<3, 6, 8, T, U>
+// where
+//     T: AddAssign + Copy + Split,
+//     U: Copy,
+// {
+//     pub fn subdivide(&self, indices: [U; 8]) -> Result<[Self; 8], OrthotreeError> {
+//         match self.kind {
+//             Kind::Leaf => {
+//                 let length = self.length.split();
+//                 let min_x = self.corner[0];
+//                 let min_y = self.corner[1];
+//                 let min_z = self.corner[2];
+//                 let mut val_x = min_x;
+//                 val_x += length;
+//                 let mut val_y = min_y;
+//                 val_y += length;
+//                 let mut val_z = min_z;
+//                 val_z += length;
+//                 let corner = self.corner;
+//                 let facets = self.facets;
+//                 Ok([
+//                     Node {
+//                         corner: [min_x, min_y, min_z],
+//                         length,
+//                         facets: [
+//                             None,
+//                             Some(indices[1]),
+//                             None,
+//                             Some(indices[2]),
+//                             None,
+//                             Some(indices[4]),
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [val_x, min_y, min_z],
+//                         length,
+//                         facets: [
+//                             Some(indices[0]),
+//                             None,
+//                             None,
+//                             Some(indices[3]),
+//                             None,
+//                             Some(indices[5]),
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [min_x, val_y, min_z],
+//                         length,
+//                         facets: [
+//                             None,
+//                             Some(indices[3]),
+//                             Some(indices[0]),
+//                             None,
+//                             None,
+//                             Some(indices[6]),
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [val_x, val_y, min_z],
+//                         length,
+//                         facets: [
+//                             Some(indices[2]),
+//                             None,
+//                             Some(indices[1]),
+//                             None,
+//                             None,
+//                             Some(indices[7]),
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [min_x, min_y, val_z],
+//                         length,
+//                         facets: [
+//                             None,
+//                             Some(indices[5]),
+//                             None,
+//                             Some(indices[6]),
+//                             Some(indices[0]),
+//                             None,
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [val_x, min_y, val_z],
+//                         length,
+//                         facets: [
+//                             Some(indices[4]),
+//                             None,
+//                             None,
+//                             Some(indices[7]),
+//                             Some(indices[1]),
+//                             None,
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [min_x, val_y, val_z],
+//                         length,
+//                         facets: [
+//                             None,
+//                             Some(indices[7]),
+//                             Some(indices[4]),
+//                             None,
+//                             Some(indices[2]),
+//                             None,
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                     Node {
+//                         corner: [val_x, val_y, val_z],
+//                         length,
+//                         facets: [
+//                             Some(indices[6]),
+//                             None,
+//                             Some(indices[5]),
+//                             None,
+//                             Some(indices[3]),
+//                             None,
+//                         ],
+//                         kind: Kind::Leaf,
+//                     },
+//                 ])
+//             }
+//             Kind::Tree(_) => Err(OrthotreeError::CannotSubdivideTree),
+//         }
+//     }
+// }
 
 // impl<const D: usize, const M: usize, const N: usize, T, U> Node<D, M, N, T, U>
 // where
