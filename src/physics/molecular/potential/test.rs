caf8ea15a2c6f205b226dfd76a2f7d1f565af3b0
mrbuche <dev@mrbuche.com>
2026-06-02 08:05:29 -0600

    cosine potential for angular and link potentials for adc

--- src/physics/molecular/potential/test.rs
+++ src/physics/molecular/potential/test.rs
@@ -2,8 +2,9 @@ use crate::math::assert::Assert;
 use crate::{
     EPSILON,
     math::{Scalar, assert::AssertionError},
-    physics::molecular::potential::{Harmonic, Morse, Potential},
+    physics::molecular::potential::{Cosine, Harmonic, Morse, Potential},
 };
+use std::f64::consts::PI;
 
 const NUM: usize = 333;
 
@@ -26,6 +27,17 @@ fn test_consistency() -> Result<(), AssertionError> {
         parameter: 1.1,
     };
     let energy = model.energy(1.51);
+    let forces = model.forces_at_energy(energy);
+    let extensions = model.extensions_at_energy(energy);
+    Assert::default().eq_within_tols(energy, &model.energy_at_force(forces[0]))?;
+    Assert::default().eq_within_tols(energy, &model.energy_at_force(forces[1]))?;
+    Assert::default().eq_within_tols(energy, &model.energy(extensions[0] + model.rest_length))?;
+    Assert::default().eq_within_tols(energy, &model.energy(extensions[1] + model.rest_length))?;
+    let model = Cosine {
+        rest_length: 1.5,
+        stiffness: 1.2,
+    };
+    let energy = model.energy(1.7);
     let forces = model.forces_at_energy(energy);
     let extensions = model.extensions_at_energy(energy);
     Assert::default().eq_within_tols(energy, &model.energy_at_force(forces[0]))?;
@@ -129,5 +141,47 @@ fn finite_difference() -> Result<(), AssertionError> {
             Assert::default().eq_within_fd_tol(compliance, &compliance_fd)?;
             Assert::default()
                 .eq_within_fd_tol(nondimensional_extension, &nondimensional_extension_fd)
+        })?;
+    let potential = Cosine {
+        rest_length: x0,
+        stiffness: e,
+    };
+    let x_max = x0 + 0.8 * 0.5 * PI;
+    (0..NUM)
+        .map(|k| x0 + (x_max - x0) * k as Scalar / NUM as Scalar)
+        .into_iter()
+        .try_for_each(|mut x| {
+            let mut force = potential.force(x);
+            let stiffness = potential.stiffness(x);
+            let anharmonicity = potential.anharmonicity(x);
+            Assert::default()
+                .eq_within_tols(potential.energy(x), &potential.energy_at_force(force))?;
+            x += 0.5 * EPSILON;
+            let mut force_fd = potential.energy(x);
+            let mut stiffness_fd = potential.force(x);
+            let mut anharmonicity_fd = potential.stiffness(x);
+            x -= EPSILON;
+            force_fd = (force_fd - potential.energy(x)) / EPSILON;
+            stiffness_fd = (stiffness_fd - potential.force(x)) / EPSILON;
+            anharmonicity_fd = (anharmonicity_fd - potential.stiffness(x)) / EPSILON;
+            Assert::default().eq_within_fd_tol(force, &force_fd)?;
+            Assert::default().eq_within_fd_tol(stiffness, &stiffness_fd)?;
+            Assert::default().eq_within_fd_tol(anharmonicity, &anharmonicity_fd)?;
+            let extension = potential.extension(force);
+            let compliance = potential.compliance(force);
+            let nondimensional_extension = potential.nondimensional_extension(force, t);
+            let nondimensional_force = potential.nondimensional_force(nondimensional_extension, t);
+            Assert::default().eq_within_tols(
+                potential.nondimensional_energy(nondimensional_extension, t),
+                &potential.nondimensional_energy_at_nondimensional_force(nondimensional_force, t),
+            )?;
+            force += 0.5 * EPSILON;
+            let mut extension_fd = potential.legendre(force);
+            let mut compliance_fd = potential.extension(force);
+            force -= EPSILON;
+            extension_fd = (potential.legendre(force) - extension_fd) / EPSILON;
+            compliance_fd = (compliance_fd - potential.extension(force)) / EPSILON;
+            Assert::default().eq_within_fd_tol(extension, &extension_fd)?;
+            Assert::default().eq_within_fd_tol(compliance, &compliance_fd)
         })
 }
