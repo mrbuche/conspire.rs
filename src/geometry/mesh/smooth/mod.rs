cb10000cee009342b79e91a89ffffac9c810e13f
mrbuche <dev@mrbuche.com>
2026-06-06 10:33:52 -0600

    smoothing options

--- src/geometry/mesh/smooth/mod.rs
+++ src/geometry/mesh/smooth/mod.rs
@@ -1,2 +1,29 @@
 mod laplace;
 mod taubin;
+
+use crate::{geometry::mesh::Mesh, math::Scalar};
+
+pub enum Smoothing {
+    Laplace {
+        iterations: usize,
+        scale: Scalar,
+    },
+    Taubin {
+        iterations: usize,
+        pass_band: Scalar,
+        scale: Scalar,
+    },
+}
+
+impl<const D: usize> Mesh<D> {
+    pub fn smooth(&mut self, smoothing: Smoothing) {
+        match smoothing {
+            Smoothing::Laplace { iterations, scale } => self.laplace_smooth(iterations, scale),
+            Smoothing::Taubin {
+                iterations,
+                pass_band,
+                scale,
+            } => self.taubin_smooth(iterations, pass_band, scale),
+        }
+    }
+}
