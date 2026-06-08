b1d410765f928732e76c76ef9a05a2d86c3c4362
mrbuche <dev@mrbuche.com>
2026-06-08 16:15:12 -0600

    edges always 2 nodes

--- src/geometry/mesh/quality/surface/mod.rs
+++ src/geometry/mesh/quality/surface/mod.rs
@@ -5,21 +5,24 @@ use crate::geometry::mesh::Mesh;
 use std::collections::HashMap;
 
 impl<const D: usize> Mesh<D> {
-    pub fn boundary_edges(&self) -> Vec<Vec<usize>> {
+    pub fn boundary_edges(&self) -> Vec<[usize; 2]> {
         self.edge_incidence()
             .into_values()
             .filter_map(|(edge, count)| (count == 1).then_some(edge))
             .collect()
     }
-    fn edge_incidence(&self) -> HashMap<Vec<usize>, (Vec<usize>, usize)> {
+    fn edge_incidence(&self) -> HashMap<[usize; 2], ([usize; 2], usize)> {
         let mut edges = HashMap::new();
         self.iter().for_each(|block| {
             let local_edges = block.local_faces();
             block.iter().for_each(|element| {
                 local_edges.iter().for_each(|edge| {
-                    let oriented: Vec<usize> = edge.iter().map(|&local| element[local]).collect();
-                    let mut key = oriented.clone();
-                    key.sort_unstable();
+                    let oriented = [element[edge[0]], element[edge[1]]];
+                    let key = if oriented[0] < oriented[1] {
+                        oriented
+                    } else {
+                        [oriented[1], oriented[0]]
+                    };
                     edges
                         .entry(key)
                         .and_modify(|(_, count)| *count += 1)
