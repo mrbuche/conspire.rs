31c5205140c26ac710b2e9b9e77535decafb2670
mrbuche <dev@mrbuche.com>
2026-09-05 20:19:24 -0600

    tidy up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![doc = include_str!("../README.md")]
-#![cfg_attr(feature = "nightly", feature(portable_simd))]
+
 #![cfg_attr(feature = "autodiff", feature(autodiff))]
+
+#![cfg_attr(feature = "nightly", feature(portable_simd))]
 
 #[cfg(feature = "constitutive")]
 pub mod constitutive;
